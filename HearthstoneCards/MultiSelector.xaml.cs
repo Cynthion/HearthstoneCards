432ea85e132638f9df0c9988f765817235910e9b
christian.luethold <christian.luethold@hotmail.com>
2015-10-20 14:01:09 +0200

    some experimentation

---
+++ HearthstoneCards/MultiSelector.xaml.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+namespace HearthstoneCards
+{
+    public sealed partial class MultiSelector : UserControl
+    {
+        public MultiSelector()
+        {
+            InitializeComponent();
+            LayoutRoot.DataContext = this;
+        }
+
+        public static readonly DependencyProperty TitleProperty =
+            DependencyProperty.Register("Title", typeof(string), typeof(MultiSelector), new PropertyMetadata(string.Empty));
+
+        public string Title
+        {
+            get { return (string)GetValue(TitleProperty); }
+            set { SetValue(TitleProperty, value); }
+        }
+    }
+}
