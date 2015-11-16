fdda6403d8dcab5c9ac419f729abbf15e181a06f
Christian Lüthold <biocodr@gmail.com>
2015-11-16 15:36:00 +0100

    styling

--- HearthstoneCards/RangeBox.xaml.cs
+++ HearthstoneCards/RangeBox.xaml.cs
@@ -15,22 +15,36 @@ namespace HearthstoneCards
             DependencyProperty.Register("Title", typeof(string), typeof(RangeBox), new PropertyMetadata(string.Empty));
 
         public static readonly DependencyProperty FromProperty =
-            DependencyProperty.Register("From", typeof(int), typeof(RangeBox), new PropertyMetadata(0, FromToValue_ChangedCallback));
-
-        private static void FromToValue_ChangedCallback(DependencyObject dObj, DependencyPropertyChangedEventArgs args)
-        {
-            var from = (int) dObj.GetValue(FromProperty);
-            var to = (int) dObj.GetValue(ToProperty);
-            dObj.SetValue(IsValidProperty, @from <= to);
-        }
+            DependencyProperty.Register("From", typeof(int), typeof(RangeBox), new PropertyMetadata(0, RangeValue_Changed));
 
         public static readonly DependencyProperty ToProperty =
-            DependencyProperty.Register("To", typeof(int), typeof(RangeBox), new PropertyMetadata(0));
+            DependencyProperty.Register("To", typeof(int), typeof(RangeBox), new PropertyMetadata(0, RangeValue_Changed));
+
+        public static readonly DependencyProperty MinProperty =
+            DependencyProperty.Register("Min", typeof(int), typeof(RangeBox), new PropertyMetadata(int.MinValue, RangeValue_Changed));
+
+        public static readonly DependencyProperty MaxProperty =
+            DependencyProperty.Register("Max", typeof(int), typeof(RangeBox), new PropertyMetadata(int.MaxValue, RangeValue_Changed));
 
         private static readonly DependencyProperty IsValidProperty =
             DependencyProperty.Register("IsValid", typeof(bool), typeof(RangeBox), new PropertyMetadata(false));
 
+        private static void RangeValue_Changed(DependencyObject dObj, DependencyPropertyChangedEventArgs args)
+        {
+            var from = (int) dObj.GetValue(FromProperty);
+            var to = (int) dObj.GetValue(ToProperty);
+            var min = (int)dObj.GetValue(MinProperty);
+            var max = (int)dObj.GetValue(MaxProperty);
 
+            // check range boundaries
+            if (from < min) dObj.SetValue(FromProperty, min);
+            if (to > max) dObj.SetValue(ToProperty, max);
+
+            // apply validity check
+            from = (int)dObj.GetValue(FromProperty);
+            to = (int)dObj.GetValue(ToProperty);
+            dObj.SetValue(IsValidProperty, @from <= to);
+        }
 
         public string Title
         {
@@ -48,6 +62,18 @@ namespace HearthstoneCards
         {
             get { return (int)GetValue(ToProperty); }
             set { SetValue(ToProperty, value); }
+        }
+
+        public int Min
+        {
+            get { return (int)GetValue(MinProperty); }
+            set { SetValue(MinProperty, value); }
+        }
+
+        public int Max
+        {
+            get { return (int)GetValue(MaxProperty); }
+            set { SetValue(MaxProperty, value); }
         }
 
         public bool IsValid
