0a5f2836af4b25bb2a592fb05750dfdad46d4a86
Christian Lüthold <biocodr@gmail.com>
2015-11-17 07:39:40 +0100

    ConstantContainer added

---
+++ HearthstoneCards/Helper/ConstantContainer.cs
@@ -0,0 +1,14 @@
+﻿namespace HearthstoneCards.Helper
+{
+    public class ConstantContainer
+    {
+        public int MinAttack { get; private set; }
+        public int MaxAttack { get; private set; }
+
+        public ConstantContainer()
+        {
+            MinAttack = 0;
+            MaxAttack = 30; // TODO check if correct
+        }
+    }
+}
