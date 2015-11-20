81b6e4fec14e6d736b1120c3962914fbe22b51a4
Christian Lüthold <biocodr@gmail.com>
2015-11-20 16:44:15 +0100

    AboutPage update

--- HearthstoneCards/AboutPage.xaml.cs
+++ HearthstoneCards/AboutPage.xaml.cs
@@ -1,10 +1,18 @@
-﻿namespace HearthstoneCards
+﻿using HearthstoneCards.ViewModel;
+using WPDevToolkit;
+
+namespace HearthstoneCards
 {
     public sealed partial class AboutPage : BasePage
     {
+        private readonly AboutViewModel _aboutVm;
+
         public AboutPage()
         {
             this.InitializeComponent();
+
+            // set data context
+            _aboutVm = SingletonLocator.Get<AboutViewModel>();
         }
     }
 }
---
+++ HearthstoneCards/ViewModel/AboutViewModel.cs
@@ -0,0 +1,16 @@
+﻿using WPDevToolkit;
+using WPDevToolkit.Interfaces;
+using WPDevToolkit.ViewModel;
+
+namespace HearthstoneCards.ViewModel
+{
+    public class AboutViewModel : BaseViewModel, ILocatable
+    {
+        public string Version { get; private set; }
+
+        public AboutViewModel()
+        {
+            Version = PhoneInteraction.GetAppVersion();
+        }
+    }
+}
